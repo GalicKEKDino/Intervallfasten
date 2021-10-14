e1f65b223f989c252475cb5db456585aacbfc4ec
Galic Dinoi <dino.galic@htl-wels.at>
2021-10-14 15:27:48 +0200

    Initial Commit

---
+++ Intervallfasten/IntervallfastenApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervallfastenApp.swift
+//  Intervallfasten
+//
+//  Created by Galic Dino on 14.10.21.
+//
+
+import SwiftUI
+
+@main
+struct IntervallfastenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
